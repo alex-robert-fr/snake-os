1c02b3d959d10151cff1ef3011ba8f87f3c8a37b
Alex Robert <contact.alexrobert04@gmail.com>
2024-02-08 14:08:00 +0100

    add lose condition

--- snake.asm
+++ snake.asm
@@ -13,6 +13,10 @@ SNAKECOLOR  equ 0x2020
 TIMER			  equ 0x046C ; Clock in BIOS
 SNAKEXARRAY equ 0x1000
 SNAKEYARRAY equ 0x2000
+UP					equ 0
+DOWN				equ 1
+LEFT				equ 2
+RIGHT 			equ 3
 
 ;; VARIABLES
 playerX: dw 40
@@ -37,6 +41,11 @@ setup_game:
 	mov word [SNAKEXARRAY], ax
 	mov ax, [playerY]
 	mov word [SNAKEYARRAY], ax
+
+	;; Hide cursor
+	mov ah, 0x02
+	mov dx, 0x2600	; DH = row, DL = col, cursor in off the visible screen
+	int 0x10
 
 ;; Game loop
 game_loop:
@@ -66,6 +75,138 @@ game_loop:
 	mov ax, APPLECOLOR
 	stosw
 
+	;; Move snake in current direction
+	mov al, [direction]
+	cmp al, UP
+	je move_up
+	cmp al, DOWN
+	je move_down
+	cmp al, LEFT
+	je move_left
+	cmp al, RIGHT
+	je move_right
+
+	jmp update_snake
+
+	move_up:
+	dec word [playerY]
+	jmp update_snake
+
+	move_down:
+	inc word [playerY]
+	jmp update_snake
+
+	move_left:
+	dec word [playerX]
+	jmp update_snake
+
+	move_right:
+	inc word [playerX]
+	jmp update_snake
+
+	;; Update snake position from playerX/Y changes
+	update_snake:
+	;; Update all snake segments past the "head", iteration back to front
+	imul bx, [snakeLength], 2 ; each array element = 2 bytes
+	.snake_loop:
+		mov ax, [SNAKEXARRAY-2+bx]
+		mov word [SNAKEXARRAY+bx], ax
+
+		dec bx
+		dec bx
+	jnz .snake_loop
+
+	;; Store updated values to head of snake in arrays
+	mov ax, [playerX]
+	mov word [SNAKEXARRAY], ax
+	mov ax, [playerY]
+	mov word [SNAKEYARRAY], ax
+
+	;; Lose conditions
+	;; 1) Hit borders of screen
+	cmp word [playerY], -1				; Top of screen
+	je game_lost
+	cmp word [playerY], SCREENH		; Bottom of screen
+	je game_lost
+	cmp word [playerX], -1				; Left of screen
+	je game_lost
+	cmp word [playerX], SCREENW		; Right of screen
+	je game_lost
+
+	;; 2) Hit part of snake
+	cmp word [snakeLength], 1			; Only have starting segment
+	je get_player_input
+
+	mov bx, 2											; Array indexes, start at 2nd array element
+	mov cx, [snakeLength]					; Loop counter
+	check_hit_snake_loop:
+		mov ax, [playerX]
+		cmp ax, [SNAKEXARRAY+bx]
+		jne .increment
+
+		mov ax, [playerY]
+		cmp ax, [SNAKEYARRAY+bx]
+		je game_lost
+
+		.increment:
+			inc bx
+			inc bx
+		loop check_hit_snake_loop
+
+	get_player_input:
+		mov bl, [direction]					; Save current direction
+
+		mov ah, 1
+		int 0x16										; Get keyboard status
+		jz check_apple							; If no key was pressed, move on
+
+		xor ah,ah
+		int 0x16										; Get keystroke, AH = scancode, AL = ascii char entered
+
+		cmp al, 'w'
+		je w_pressed
+		cmp al, 's'
+		je s_pressed
+		cmp al, 'a'
+		je a_pressed
+		cmp al, 'd'
+		je d_pressed
+
+		jmp check_apple
+
+		w_pressed:
+			mov bl, UP
+			jmp check_apple
+
+		s_pressed:
+			mov bl, DOWN
+			jmp check_apple
+
+		a_pressed:
+			mov bl, LEFT
+			jmp check_apple
+
+		d_pressed:
+			mov bl, RIGHT
+			jmp check_apple
+
+	;; Did player hit apple ?
+	check_apple:
+		mov byte [direction], bl		; Update direction
+
+		mov ax, [playerX]
+		cmp ax, [appleX]
+		jne delay_loop
+
+		mov ax, [playerY]
+		cmp ax, [appleY]
+		jne delay_loop
+
+		; Hit apple, increase snake length
+		inc word [snakeLength]
+		cmp word [snakeLength], WINCOND
+
+
 	delay_loop:
 		mov bx, [TIMER]
 		inc bx
@@ -74,8 +215,24 @@ game_loop:
 			cmp [TIMER], bx
 			jl .delay
 
-
 jmp game_loop
+
+;; End conditions
+game_won:
+	jmp reset
+
+game_lost:
+	mov dword [ES:0000], 0x1F4F1F4C	; Lo
+	mov dword [ES:0004], 0x1F451F53; se
+
+;; Reset the game
+reset:
+	xor ah, ah
+	int 0x16
+
+	jmp 0x0FFFF:0x0000			; Far jump to reset vector, "warm reboot"
+;;	int 0x19							; Alternative reset, restarts qemu
+
 ;; Bootsector padding
 times 510 - ($-$$) db 0
 
